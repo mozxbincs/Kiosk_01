9fd14e6f63ab87a2e8661bb8745acc94cf1690a6
kimhakyu <kimhakyu0610@gmail.com>
2024-09-30 06:45:58 +0900

    TextWathcer 설정 이메일, 사업자번호 중복 확인 오류 발생

--- app/src/main/java/com/example/kiosk02/admin/AdminSignFragment.kt
+++ app/src/main/java/com/example/kiosk02/admin/AdminSignFragment.kt
@@ -1,12 +1,16 @@
 package com.example.kiosk02.admin
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.kiosk02.R
@@ -16,6 +20,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.regex.Pattern
+
 class AdminSignFragment : Fragment(R.layout.activity_admin_sign) {
 
     private lateinit var binding: ActivityAdminSignBinding
@@ -24,6 +30,9 @@ class AdminSignFragment : Fragment(R.layout.activity_admin_sign) {
     private lateinit var firestore: FirebaseFirestore
     private val apiKey = "q40rN3NyDh2wW96K08F2X3EGCIyLsV8c3tbNBgnZBZ3hFmxCeznuLoJr5+aHtlHRRH0GA+NpGakhiX13jv4pWg=="
     private var businessNumberCk = false
+
+    private var isEmailUnique = true
+    private var isBusinessNumberUnique = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -39,11 +48,163 @@ class AdminSignFragment : Fragment(R.layout.activity_admin_sign) {
             checkBusinessNumber(businessNumber)
         }
 
-        // 관리자 회원가입 전 버튼 클릭 리스너 설정
-        view.findViewById<Button>(R.id.registerBackButton).setOnClickListener {
-            findNavController().navigate(R.id.action_to_adminFragment) // 관리자 초기화면으로 이동
-        }
+        binding.nameEditText.addTextChangedListener(textWatcher)
+        binding.emailEditText.addTextChangedListener(textWatcher)
+        binding.passwordEditText.addTextChangedListener(textWatcher)
+        binding.confirmPasswordEditText.addTextChangedListener(textWatcher)
+        binding.businessNameEditText.addTextChangedListener(textWatcher)
+        binding.businessNumberEditText.addTextChangedListener(textWatcher)
+        binding.addressEditText.addTextChangedListener(textWatcher)
+        binding.startEditText.addTextChangedListener(textWatcher)
+
     }
+
+    // 정규식 지정
+    private val emailPattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$\$".toRegex()
+    private val passwordPattern = "^.*(?=^.{8,15}\$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#\$%^&+=]).*\$".toRegex()
+    private val hangulPatten = "^[가-힣]{2,}\$".toRegex()
+    private val businessnumberPatten = "^\\d{10}\$".toRegex()
+    //private val adressPatten = "^[가-힣a-zA-Z0-9\\\\s,.-]{3,}\$".toRegex()
+    private val startDayPatten = "^\\d{8}\$".toRegex()
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            val editText = view?.findFocus() as? EditText
+            editText?.let { validateInputs(it) }
+        }
+
+        override fun afterTextChanged(p0: Editable?) {}
+
+    }
+    // 검사 기능
+    private fun validateInputs(editText: EditText) {
+        when(editText.id) {
+            R.id.nameEditText -> {
+                val name = binding.nameEditText.text.toString()
+                val NameValid = name.matches(hangulPatten)
+                if (NameValid != true) {
+                    binding.warningTextView.text = getString(R.string.warning_hangul)
+                    binding.warningTextView.visibility = View.VISIBLE
+                } else {
+                    binding.warningTextView.visibility = View.GONE
+                }
+            }
+
+            R.id.emailEditText -> {
+                val email = binding.emailEditText.text.toString()
+                val Emailvalid = email.matches(emailPattern)
+                if(Emailvalid != true) {
+                    binding.warningTextView.text = getString(R.string.warning_email)
+                    binding.warningTextView.visibility = View.VISIBLE
+                } else {
+                    binding.warningTextView.visibility = View.GONE
+                    //checkEmailInFirebase(email)
+                }
+            }
+
+            R.id.passwordEditText -> {
+                val password = binding.passwordEditText.text.toString()
+                val passwordValid = password.matches(passwordPattern)
+                if (passwordValid != true) {
+                    binding.warningTextView.text = getString(R.string.warning_password)
+                    binding.warningTextView.visibility = View.VISIBLE
+                } else {
+                    binding.warningTextView.visibility = View.GONE
+                }
+            }
+
+            R.id.confirmPasswordEditText -> {
+                val confirmpassword = binding.confirmPasswordEditText.text.toString()
+                if (confirmpassword != binding.passwordEditText.text.toString()) {
+                    binding.warningTextView.text = "비밀번호가 일치하지 않습니다."
+                    binding.warningTextView.visibility = View.VISIBLE
+                } else {
+                    binding.warningTextView.visibility = View.GONE
+                }
+            }
+
+            R.id.businessNameEditText -> {
+                val businessname = binding.businessNameEditText.text.toString()
+                val NameValid = businessname.matches(hangulPatten)
+                if (NameValid != true) {
+                    binding.warningTextView.text = getString(R.string.warning_hangul)
+                    binding.warningTextView.visibility = View.VISIBLE
+                } else {
+                    binding.warningTextView.visibility = View.GONE
+                }
+            }
+
+            R.id.businessNumberEditText -> {
+                val businessnumber = binding.businessNumberEditText.text.toString()
+                val businessnumbervalid = businessnumber.matches(businessnumberPatten)
+                if(businessnumbervalid != true) {
+                    binding.warningTextView.text = getString(R.string.warning_businessnumber)
+                    binding.warningTextView.visibility = View.VISIBLE
+                } else {
+                    binding.warningTextView.visibility = View.GONE
+                    //checkBusinessNumberInFirebase(businessnumber)
+                }
+            }
+
+            R.id.addressEditText -> {
+                val address = binding.addressEditText.text.toString()
+                val NameValid = address.matches(hangulPatten)
+                if (NameValid != true) {
+                    binding.warningTextView.text = getString(R.string.warning_hangul)
+                    binding.warningTextView.visibility = View.VISIBLE
+                } else {
+                    binding.warningTextView.visibility = View.GONE
+                }
+            }
+
+            R.id.startEditText -> {
+                val start = binding.startEditText.text.toString()
+                val NameValid = start.matches(startDayPatten)
+                if (NameValid != true) {
+                    binding.warningTextView.text = getString(R.string.warning_startday)
+                    binding.warningTextView.visibility = View.VISIBLE
+                } else {
+                    binding.warningTextView.visibility = View.GONE
+                }
+            }
+        }
+        isAllInputsValid()
+    }
+
+    private fun isAllInputsValid(): Boolean {
+        val isWarningHidden = binding.warningTextView.visibility == View.GONE
+
+        return isWarningHidden
+    }
+    /*
+    private fun checkEmailInFirebase(email: String) {
+        firestore.collection("admin")
+            .whereEqualTo("email", email)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents.isEmpty) {
+                    isEmailUnique = false }
+                validateInputs(binding.emailEditText)
+            }.addOnFailureListener { documents ->
+                isEmailUnique = false
+                validateInputs(binding.emailEditText)
+            }
+    }
+
+    private fun checkBusinessNumberInFirebase(businessnumber: String) {
+        firestore.collection("admin")
+            .whereEqualTo("businessnumber", businessnumber)
+            .get()
+            .addOnSuccessListener { documents ->
+                isBusinessNumberUnique = documents.isEmpty
+                //validateInputs(binding.businessNumberEditText)
+            }.addOnFailureListener {
+                isBusinessNumberUnique = false
+                //validateInputs(binding.businessNumberEditText)
+            }
+    }*/
 
     private fun registerUser(name: String, email: String, password: String, businessname: String, businessnumber: String, address: String) {
         auth.createUserWithEmailAndPassword(email, password)
