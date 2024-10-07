daf392f3a3bdb275dff4d1786575caf95bb8b06c
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-10-07 20:25:06 +0900

    [Feat] 메뉴 리스트 화면 생성

---
+++ app/src/main/java/com/example/kiosk02/admin/AdminMenuListFragment.kt
@@ -0,0 +1,14 @@
+package com.example.kiosk02.admin
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.kiosk02.databinding.FragmentAdminMenuListBinding
+
+class AdminMenuListFragment: Fragment() {
+    private lateinit var binding: FragmentAdminMenuListBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
