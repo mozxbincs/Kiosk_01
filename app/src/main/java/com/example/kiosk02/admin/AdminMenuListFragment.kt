a526d885167ae8aec74adb3508ffa3711b447d06
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-10-09 14:24:52 +0900

    [Feat] RecyclerView item_menu 2열로 배치

--- app/src/main/java/com/example/kiosk02/admin/AdminMenuListFragment.kt
+++ app/src/main/java/com/example/kiosk02/admin/AdminMenuListFragment.kt
@@ -3,6 +3,7 @@ package com.example.kiosk02.admin
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.kiosk02.databinding.FragmentAdminMenuListBinding
 
 class AdminMenuListFragment: Fragment() {
@@ -10,5 +11,12 @@ class AdminMenuListFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentAdminMenuListBinding.bind(view)
+
+        //item_menu 2줄 나열
+        val recyclerView = binding.menuListRecyclerView
+        val gridLayoutManager = GridLayoutManager(requireContext(),2)
+        recyclerView.layoutManager = gridLayoutManager
     }
 }
